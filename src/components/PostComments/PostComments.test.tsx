8fcbdd91c91e78f06c46f132904691af0df455e7
Bruno Dambroski <bruno3414.dambroski@gmail.com>
2024-09-16 19:45:30 -0400

    Adicionado teste para 2 comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import App from '../../App';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<App/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+
+    it('Teste para adicionar 2 comentários', () => {
+        const { debug } = render(<App />)
+
+        const fireEventInput = (valor: string) => {
+            fireEvent.change(screen.getByTestId('campo-comentario'), {
+                target : {
+                    value: valor
+                }
+            })
+            fireEvent.click(screen.getByTestId('btn-comentar'))
+            expect(screen.getByText(valor)).toBeInTheDocument()
+        }
+        fireEventInput('teste')
+        fireEventInput('teste2')
+        // eslint-disable-next-line testing-library/no-debugging-utils
+        debug()
+    })
+    ;
 });
